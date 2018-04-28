271d705ce0b3488b1a12169e3d3631232d785d7e
Arjun Mahishi <arjun.mahishi@gmail.com>
2018-04-28 18:01:20 +0530

    Added firebase stuff

--- script.js
+++ script.js
@@ -8,15 +8,40 @@ catch(e) {
   // $('.app').hide();
 }
 
+
+var config = {
+  apiKey: "AIzaSyAJQaVysxwNwEcytdDm-CULQqC998s3PVo",
+  authDomain: "learn1-d280c.firebaseapp.com",
+  databaseURL: "https://learn1-d280c.firebaseio.com",
+  projectId: "learn1-d280c",
+  storageBucket: "learn1-d280c.appspot.com",
+  messagingSenderId: "708667620662"
+};
+
+firebase.initializeApp(config);
+var database=firebase.database();
+
 //// methods ////
 const startListening = () => recognition.start();
 const stopListening = () => recognition.stop();
+
+const sendMessage = ( sender, reciever, message) => {  
+    var info=database.ref("/messages");
+    info.push({ 
+	    sender: sender,
+	    reciever: reciever,
+	    message: message
+	})
+}
 
 //// Voice Recognition //// 
 // recognition.continuous = false;
 // recognition.interimResults = true;
 
 recognition.onresult = function(event) {
+
+  // console.log(event)
+
   var current = event.resultIndex;
 
   var transcript = event.results[current][0].transcript;
@@ -24,7 +49,9 @@ recognition.onresult = function(event) {
   var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
 
   if(!mobileRepeatBug) {
-    $(".header").html(transcript);
+    // $(".header").html(transcript);
+    sendMessage("me", "him", transcript)
+
   }
 };
 
@@ -60,3 +87,7 @@ const speak = (message) => {
 window.onload = () => {
   startListening();
 }
+
+database.ref("/messages").on('child_added', (data) => {
+    document.querySelector(".header").innerHTML += `<br /> * ${data.val().message}`;  
+});
