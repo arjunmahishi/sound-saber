5e1529d01a11bbd11a5d7c592d2523b24bb1231a
Arjun Mahishi <arjun.mahishi@gmail.com>
2018-04-29 01:57:26 +0530

    Added google auth

---
+++ js/auth.js
@@ -0,0 +1,57 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+
+const login = () => {
+	firebase.auth().signInWithRedirect(provider).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  var user = result.user;
+		console.log(user);
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+		console.log(errorMessage);
+	  // ...
+	});
+
+	firebase.auth().getRedirectResult().then(function(result) {
+	  if (result.credential) {
+	    // This gives you a Google Access Token. You can use it to access the Google API.
+	    var token = result.credential.accessToken;
+	    // ...
+			// window.location = "/profile/";
+	  }
+	  // The signed-in user info.
+	  var user = result.user;
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+		console.log(errorMessage);
+	});
+}
+
+const logout = () => {
+	firebase.auth().signOut();
+}
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("Logged in!");
+		window.location = "recent.html";
+  } else {
+    console.log("didn't work");
+  }
+});
