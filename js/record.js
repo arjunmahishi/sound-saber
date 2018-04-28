288b94cca732c9f5f4eedfa34180e689f2cae288
Arjun Mahishi <arjun.mahishi@gmail.com>
2018-04-28 22:29:39 +0530

    Added record functionality

---
+++ js/record.js
@@ -0,0 +1,29 @@
+var SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+
+//// Microphone controls //// 
+const startListening = () => recognition.start();
+const stopListening = () => recognition.stop();
+
+recognition.onresult = function(event) {
+    var current = event.resultIndex;
+    var transcript = event.results[current][0].transcript;
+    var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
+
+    if(!mobileRepeatBug) sendMessage("me", "him", transcript);
+
+};
+
+recognition.onstart = function() { 
+    console.log("Started recording!")
+}
+
+recognition.onend = function() {
+    recognition.start();
+}
+
+recognition.onerror = function(event) {
+    if(event.error == 'no-speech') {
+    };
+}
